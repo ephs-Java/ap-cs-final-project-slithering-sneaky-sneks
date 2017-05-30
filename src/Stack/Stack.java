f06ce4762ed9332a4fef8b7d71ffbd6cbcfb3e6c
ace3449 <90299447@hs-05669-macair.ad.edenpr.org>
2017-05-30 11:26:00 -0500

    here

--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -1,5 +1,47 @@
 package Stack;
 
-public class Stack {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+
+public class Stack extends JFrame {
+	private int xCord1;
+	private int yCord1;
+	private int width1;
+	private int xVel;
+	
+	public Stack() {
+		this.xCord1 = 100;
+		this.yCord1 = 200;
+		this.width1 = 10;
+		this.xVel = 10;
+		JFrame jf = new JFrame("Stack");
+		setSize(1200, 500);
+		setResizable(true);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+		g.setColor(Color.black);
+		g.fillRect(xCord1 + xVel, yCord1, width1, 50);
+		
+	}
+	
+	public void move() {
+		this.xCord1 = this.xCord1 + this.xVel;
+		repaint();
+	}
+	
+	public int getXCord() {
+		return this.xCord1;
+	}
+
+	
+	
 }
--- src/Stack/StackRunner.java
+++ src/Stack/StackRunner.java
@@ -3,8 +3,17 @@ package Stack;
 public class StackRunner {
 
 	public static void main(String[] args) {
-		
-
+		Stack stack = new Stack();
+		while(stack.getXCord() < 800) {
+			stack.move();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(stack.getXCord());
+		}
 	}
 
 }
